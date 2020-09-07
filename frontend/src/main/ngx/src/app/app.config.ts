00c43aa17cc4484368b729cd95ab443c5ae2976d
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2020-09-07 15:27:02 +0200

    Updated ontimize web to version 8

--- frontend/src/main/ngx/src/app/app.config.ts
+++ frontend/src/main/ngx/src/app/app.config.ts
@@ -1,28 +1,26 @@
 import { Config } from 'ontimize-web-ngx';
 
-import { SERVICE_CONFIG } from './shared/app.services.config';
 import { MENU_CONFIG } from './shared/app.menu.config';
+import { SERVICE_CONFIG } from './shared/app.services.config';
 
 export const CONFIG: Config = {
   // The base path of the URL used by app services.
-  apiEndpoint: 'http://localhost:33333',
-  bundle: {
-    path: 'bundle'
-  },
+  apiEndpoint: '[COMPLETE-WITH-YOUR-REST-API]',
+
   // Application identifier. Is the unique package identifier of the app.
   // It is used when storing or managing temporal data related with the app.
   // By default is set as 'ontimize-web-uuid'.
-  uuid: 'com.ontimize.web.ngx.jee.seed',
+  uuid: 'com.ontimize.web.ngx.seed',
 
   // Title of the app
-  title: 'JEE seed',
+  title: 'Ontimize Web Seed',
 
   //  Language of the application.
   locale: 'en',
 
   // The service type used (Ontimize REST standart, Ontimize REST JEE
   // or custom implementation) in the whole application.
-  serviceType: 'OntimizeEE',
+  //serviceType: 'OntimizeEE',
 
   // Configuration parameters of application services.
   servicesConfiguration: SERVICE_CONFIG,
