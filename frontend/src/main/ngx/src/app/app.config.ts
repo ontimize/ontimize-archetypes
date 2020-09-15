c1e7f56de76cda6422fbe9381df168bcd1f6b04b
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2020-09-15 19:14:26 +0200

    Added apiEndpoint

--- frontend/src/main/ngx/src/app/app.config.ts
+++ frontend/src/main/ngx/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { SERVICE_CONFIG } from './shared/app.services.config';
 
 export const CONFIG: Config = {
   // The base path of the URL used by app services.
-  apiEndpoint: '[COMPLETE-WITH-YOUR-REST-API]',
+  apiEndpoint: 'http://localhost:33333',
 
   // Application identifier. Is the unique package identifier of the app.
   // It is used when storing or managing temporal data related with the app.
