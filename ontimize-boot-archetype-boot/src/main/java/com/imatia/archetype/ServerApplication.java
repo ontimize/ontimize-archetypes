a1efef77b9a62a2da286cef7eb8454df982763b8
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-05-14 12:38:15 +0200

    Renamed packages, modules and removed componentscan

---
+++ ontimize-boot-archetype-boot/src/main/java/com/imatia/archetype/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.imatia.archetype;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
