a2baee2c0381e2127e9a3a31405d91cb8aaa0473
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-11-22 16:47:57 +0100

    Update ServerApplication.java

--- ontimize-boot-archetype-boot/src/main/java/com/imatia/archetype/ServerApplication.java
+++ ontimize-boot-archetype-boot/src/main/java/com/imatia/archetype/ServerApplication.java
@@ -1,9 +1,7 @@
 package com.imatia.archetype;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class ServerApplication {
